4bb2ea23d6a4607cc5bf0ad12755cd0d3979d714
Harshil-Vaghasiya <harshilvaghasiya25@gmail.com>
2023-12-26 12:03:04 +0530

    set db_url to env

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,8 @@
 import express, {Request, Response} from 'express';
 import mongoose from 'mongoose';
+import {config} from 'dotenv';
+config();
 
-import DeckModal from './models/Deck';
 import Deck from './models/Deck';
 
 const PORT = 8000;
@@ -12,14 +13,13 @@ app.use(express.json());
 
 app.post('/decks', async (req: Request, res: Response) => {
 
-  console.log('body', req.body)
   const newDeck = new Deck(req.body)
 
   const createdDeck = await newDeck.save();
   res.send(createdDeck)
 })
 
-mongoose.connect('mongodb+srv://harshil:iYbqUpUwE2LOrBGP@cluster0.oi4zo1n.mongodb.net/').then(() => {
+mongoose.connect(process.env.MONGO_URL!).then(() => {
   console.log(`listening on port ${PORT}`)
   app.listen(PORT)
 });
